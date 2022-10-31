d4b0742e59ef7247054c3087c94b5af8dd1aacf1
oritise1 <ikareemabiodun730@gmail.com>
2022-10-31 22:09:21 +0800

    create react app for the linktree project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/Button';
+
+const App = () => {
+  const item =[
+    {
+      textName : 'Twitter',
+      link : 'https://twitter.com/oritise_success'
+    }
+  ]
+  return (
+    
+    <div className="App">
+      
+    <h1>HNG9 LINKTREE TASK</h1>
+    </div>
+  );
+}
+
+export default App;
